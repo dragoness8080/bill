3b6867f65e93f863950fd557f8842874b21f16ef
devil5050 <devil5050@126.com>
2018-02-24 22:27:35 +0800

    后台首页

---
+++ application/admin/controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Administrator
+     * Date: 2018/2/24
+     * Time: 22:26
+     */
+
+    namespace app\admin\controller;
+
+
+    use app\admin\controller\base\BaseController;
+
+    class IndexController extends BaseController {
+
+        public function index(){
+
+        }
+    }
