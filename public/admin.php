294fd51fce13b9668dbbd20645d6dfee6beab60a
devil5050 <devil5050@126.com>
2018-02-24 21:05:28 +0800

    后台首页

---
+++ public/admin.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Administrator
+     * Date: 2018/2/24
+     * Time: 21:02
+     */
+
+    define('APP_PATH', __DIR__ . '/../application/');
+    define('BIND_MODEL', 'admin');
+    require __DIR__ . '/../thinkphp/start.php';
