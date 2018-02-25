118286ffdd8bad4ce3e51bb213e22b50fa9ccc14
devil5050 <devil5050@126.com>
2018-02-25 23:21:43 +0800

    add by 2018-02-25

---
+++ application/admin/config.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Administrator
+     * Date: 2018/2/25
+     * Time: 23:18
+     */
+
+    return [
+        'template'  => [
+            'layout_on'     => true,
+            'layout_name'   => 'layout/main_layout',
+            //'layout_item'   => '{__CONTENT__}'
+        ]
+    ];
