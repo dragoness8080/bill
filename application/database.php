1845583d45b49105cc4956afeee14d25685fdadb
devil5060 <devil5060@hotmail.com>
2018-02-25 13:52:17 +0800

    add by 2018-02-25

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '118.89.193.26',
     // 数据库名
-    'database'        => '',
+    'database'        => 'bill',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'bill_user',
     // 密码
-    'password'        => '',
+    'password'        => '123456',
     // 端口
-    'hostport'        => '',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
