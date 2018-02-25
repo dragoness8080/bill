cc496e832166007383ece164c5c075340a4a68c4
devil5060 <devil5060@hotmail.com>
2018-02-25 11:09:53 +0800

    后台牵引页面2018-02-25

---
+++ public/admin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: appleimac
+ * Date: 18/2/25
+ * Time: 上午11:07
+ */
+
+define('APP_PATH', __DIR__ . '/../application/');
+define('BIND_MODULE', 'admin');
+require __DIR__ . '/../thinkphp/start.php';
